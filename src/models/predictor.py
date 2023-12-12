782d74a796fa85707d6de65121dbb5e2f4057533
patenvy1234 <83762723+patenvy1234@users.noreply.github.com>
2023-12-12 18:51:08 +0530

    Update predictor.py

--- src/models/predictor.py
+++ src/models/predictor.py
@@ -317,7 +317,7 @@ class Translator(object):
             topk_log_probs = topk_scores * length_penalty
 
             # Resolve beam origin and true word ids.
-            topk_beam_index = topk_ids.div(vocab_size)
+            topk_beam_index = topk_ids // vocab_size
             topk_ids = topk_ids.fmod(vocab_size)
 
             # Map beam_index to batch_index in the flat representation.
